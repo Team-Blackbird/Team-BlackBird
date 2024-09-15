dcc4f56858527690dfc8c32a451470faa6918abf
purple wazard <purplewazard@gmail.com>
2024-09-15 10:11:38 -0500

    multi placeable card positions

--- scripts/cards.gd
+++ scripts/cards.gd
@@ -6,7 +6,10 @@ func _physics_process(delta):
 	if is_selected:
 		position = lerp(global_position, get_global_mouse_position(), 25 * delta )
 	if $Area2D.has_overlapping_areas() and not is_selected:
-		position = lerp(position, $Area2D.get_overlapping_areas()[0].global_position, 25 * delta)
+		var over_area = $Area2D.get_overlapping_areas()[0]
+		if over_area.get("is_placeable_area"):
+			if over_area.is_placeable_area:
+				position = lerp(position, over_area.global_position, 25 * delta)
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
@@ -15,7 +18,3 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 		is_selected = false
-		
-			
-		
-	
---
+++ scripts/placeable_area.gd
@@ -0,0 +1,4 @@
+extends Area2D
+var has_card = false
+var is_placeable_area = true
+var type = "normal"
